783fae2f3b7083b5638985f4ea0ed0606c8aa66e
Faruk Emre <faruk.eksioglu@hotmail.com>
2020-10-12 17:41:55 +0300

    change main fragment label, add menu for link to linkedin and google play store

--- app/src/main/java/dev/faruke/helperclock/view/MainActivity.kt
+++ app/src/main/java/dev/faruke/helperclock/view/MainActivity.kt
@@ -2,22 +2,29 @@ package dev.faruke.helperclock.view
 
 import android.content.Intent
 import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuBuilder
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
 import dev.faruke.helperclock.R
 import dev.faruke.helperclock.service.FakeTimeService.Companion.mainActivity
 import dev.faruke.helperclock.service.FakeTimeService.Companion.mainFragmentViewModel
-import dev.faruke.helperclock.view.customViews.RingClockCheckbox
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.reflect.Method
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,6 +51,31 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.fragment)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when(id) {
+            R.id.menu_linkedin -> {
+                val browserIntent =
+                    Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/femrek/"))
+                startActivity(browserIntent)
+                return true
+            }
+            R.id.menu_google_play -> {
+                val browserIntent =
+                    Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/dev?id=7986243585950801287"))
+                startActivity(browserIntent)
+                return true
+            }
+        }
+        return false
     }
 
     fun addClickListener(view: View?) {
